f79cd5b97c0c9d993cedf319fd3e7beef964c800
Salman Thaikat <salmanthaikkat@gmail.com>
2018-07-02 01:00:09 +0530

    Added Product model

---
+++ models/product.js
@@ -0,0 +1,26 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+
+var productSchema=new Schema({
+  title:{
+    type:String,
+    required:true
+  },
+  desc:{
+    tpe:String,
+    required:true
+  },
+  category:{
+    type:String,
+    required:true
+  },
+  price:{
+    type:Number,
+    required:true
+  },
+  image:{
+    type:String
+  }
+});
+
+module.exports=mongoose.model('Product',productSchema);
